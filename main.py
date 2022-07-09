124bf2dbd048e7f409fdf571965061394271a6b8
André Bruni <andrefcbruni@gmail.com>
2022-07-09 09:59:11 -0300

    refactor: new Window class containing everything

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
+from argparse import Action
 from PySide6.QtCore import Qt 
-from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout, QMenuBar
+from PySide6.QtGui import QFont, QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        window = QMainWindow()
+        base = QWidget()
+        layout = QVBoxLayout()
+        font = QFont()
+        font.setPixelSize(30)
+        label = QLabel("Primeira label aqui.")
+        label.setFont(font)
+        label.setAlignment(Qt.AlignCenter)
+        botao = QPushButton("Botão aqui.")
+        botao.setFont(font)
+        layout.addWidget (label)
+        layout.addWidget (botao)
+        base.setLayout(layout)
+        base.show()
+        self.setCentralWidget(base)
+        menu = self.menuBar()
+        file_menu = menu.addMenu("File")
+        action = QAction("Print!")
+        file_menu.addAction(action)    
 
 app = QApplication()
-window = QMainWindow()
-base = QWidget()
-layout = QVBoxLayout()
-
-font = QFont()
-font.setPixelSize(30)
-
-label = QLabel("Primeira label aqui.")
-label.setFont(font)
-label.setAlignment(Qt.AlignCenter)
-
-botao = QPushButton("Botão aqui.")
-botao.setFont(font)
-
-layout.addWidget (label)
-layout.addWidget (botao)
-
-base.setLayout(layout)
-base.show()
-
-window.setCentralWidget(base)
-menu = window.menuBar()
-menu.addMenu("File")
-
+window = Window()
 window.show()
 app.exec()
