304c3608b2524261a8de9f5c2cc1046ae04d65d0
André Bruni <andrefcbruni@gmail.com>
2022-07-09 09:28:00 -0300

    feature: changing fonts and alignments

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from PySide6.QtCore import Qt 
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel
 
 app = QApplication()
 
+font = QFont()
+font.setPixelSize(30)
+
 label = QLabel("Primeira label aqui.")
+label.setFont(font)
+label.setAlignment(Qt.AlignCenter)
 label.show()
+
 
 app.exec()
