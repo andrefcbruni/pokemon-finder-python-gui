5cc192d6f84cf40c8b00aededaaa2d99cefbf595
André Bruni <andrefcbruni@gmail.com>
2022-07-09 09:38:15 -0300

    fix: all widgets in the same screen

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PySide6.QtCore import Qt 
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 app = QApplication()
+base = QWidget()
+layout = QVBoxLayout()
 
 font = QFont()
 font.setPixelSize(30)
@@ -10,7 +12,13 @@ font.setPixelSize(30)
 label = QLabel("Primeira label aqui.")
 label.setFont(font)
 label.setAlignment(Qt.AlignCenter)
-label.show()
 
+botao = QPushButton("Botão aqui.")
+botao.setFont(font)
 
+layout.addWidget (label)
+layout.addWidget (botao)
+
+base.setLayout(layout)
+base.show()
 app.exec()
