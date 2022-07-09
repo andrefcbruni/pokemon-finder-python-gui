2e1f067db551f93f375fabb367fdfa4042cdee81
André Bruni <andrefcbruni@gmail.com>
2022-07-09 13:23:25 -0300

    documentation: comments added in pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -5,13 +5,13 @@ from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtGui import QGuiApplication
 from httpx import get
 
+# It fetches the image of a pokemon from the PokeAPI and saves it open using QML:
 class Bridge(QObject):
     @Slot(str, result = list)
     def fetch_image(self, pokemon_id):
         image_path = Path('pokemon_image.png')
         try:
             if image_path.exists():
-               #print('Deletando imagem...')
                image_path.unlink()
             response = get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}').json()
             image_url = response['sprites']['other']['home']['front_default']
@@ -23,6 +23,8 @@ class Bridge(QObject):
         finally:
             return str(image_path), response['name'].capitalize()
 
+# Creating a QGuiApplication, loading the QML file, creating a bridge, setting the context property,
+# and executing the application.
 app = QGuiApplication()
 engine = QQmlApplicationEngine()
 engine.load('pokemon.qml')
